49250c3702f8cb2c31f18a8a0d58a5a8a373bc12
mike <mikefc@coolbutuseless.com>
2024-04-12 18:06:11 +1000

    smuggled read

--- src/serialize_con_smuggle.c
+++ src/serialize_con_smuggle.c
@@ -22,12 +22,12 @@ SEXP init_smuggle_(SEXP rc_xptr) {
   return R_NilValue;
 }
 
-// size_t read_connection(SEXP connection, void* buf, size_t n) {
-  // auto read_connection_ptr = reinterpret_cast<size_t (*)(SEXP, void*, size_t)>(
-    // R_ExternalPtrAddr(read_connection_xptr));
-  // (size_t (*)(SEXP, void*, size_t))func = (size_t (*)(SEXP, void*, size_t))read_connection_xptr;
-  // return (size_t (*)(SEXP, void*, size_t))(read_connection_xptr)(connection, buf, n);
-// }
+size_t read_connection(SEXP connection, void* buf, size_t n) {
+// auto read_connection_ptr = reinterpret_cast<size_t (*)(SEXP, void*, size_t)>(R_ExternalPtrAddr(read_connection_xptr));
+size_t (*read_connection)(SEXP, void*, size_t);
+  read_connection = (size_t (*)(SEXP, void *, size_t))R_ExternalPtrAddr(read_connection_xptr);
+  return read_connection(connection, buf, n);
+}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Read a byte from the serialized stream
@@ -43,9 +43,9 @@ int read_byte_from_connection_smuggle(R_inpstream_t stream) {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void read_bytes_from_connection_smuggle(R_inpstream_t stream, void *dst, int length) {
   
-  Rconnection rcon = (Rconnection)stream->data;
-  size_t nread = R_ReadConnection(rcon, dst, length);
-  // size_t nread = read_connection(stream->data, dst, length);
+  // Rconnection rcon = (Rconnection)stream->data;
+  // size_t nread = R_ReadConnection(rcon, dst, length);
+  size_t nread = read_connection(stream->data, dst, length);
 
   // Sanity check that we read the requested number of bytes from the connection
   if (nread != length) {
@@ -76,14 +76,12 @@ void write_bytes_to_connection_smuggle(R_outpstream_t stream, void *src, int len
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 SEXP unmarshall_con_smuggle_(SEXP con_) {
   
-  Rconnection rcon = R_GetConnection(con_);
-  
   // Treat the data buffer as an input stream
   struct R_inpstream_st input_stream;
 
   R_InitInPStream(
     &input_stream,                 // Stream object wrapping data buffer
-    (R_pstream_data_t) rcon,       // the user data is just a (void *) to the connection
+    (R_pstream_data_t) con_,       // the user data is just a (void *) to the connection
     R_pstream_any_format,          // Unpack all serialized types
     read_byte_from_connection_smuggle,     // Function to read single byte from buffer
     read_bytes_from_connection_smuggle,    // Function for reading multiple bytes from buffer
@@ -102,15 +100,13 @@ SEXP unmarshall_con_smuggle_(SEXP con_) {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 SEXP marshall_con_smuggle_(SEXP robj, SEXP con_) {
   
-  Rconnection rcon = R_GetConnection(con_);
-  
   // Create the output stream structure
   struct R_outpstream_st output_stream;
   
   // Initialise the output stream structure
   R_InitOutPStream(
     &output_stream,               // The stream object which wraps everything
-    (R_pstream_data_t) rcon,      // the user data is just a (void *) to the connection
+    (R_pstream_data_t) con_,      // the user data is just a (void *) to the connection
     R_pstream_binary_format,      // Store as binary
     3,                            // Version = 3 for R >3.5.0 See `?base::serialize`
     write_byte_to_connection_smuggle,     // Function to write single byte to buffer
